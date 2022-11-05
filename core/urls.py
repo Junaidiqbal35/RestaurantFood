0d28fcf1ccda288163cf80f04396badaffc7f915
Junaid Iqbal <junaidiqbal0323@gmail.com>
2022-11-05 15:12:51 +0500

    new update

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 from .views import TableReservationView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="core/landing_page.html"), name='landing-page'),
+    path('menu/', TemplateView.as_view(template_name="core/menu.html"), name='menu'),
     path('table/reservation/', TableReservationView.as_view(), name='table-reservation'),
 ]
